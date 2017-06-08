41d002363cbf8fa7b3f922b1def083e8b2af2168
barrowsx <wwboyer14@mail.strakejesuit.org>
2017-06-08 09:39:13 -0400

    Classy

---
+++ lib/dog.rb
@@ -0,0 +1,7 @@
+class Dog
+  # TODO: literally anything
+end
+
+fido = Dog.new
+snoopy = Dog.new
+lassie = Dog.new
---
+++ lib/person.rb
@@ -0,0 +1,6 @@
+class Person
+  # TODO: things
+end
+
+adele_goldberg = Person.new
+alan_kay = Person.new
